5f2d38932d9d62e936e72f02a0b7eba4aad66598
kalinduagathisi <kalinduagathisi@gmail.com>
2025-02-26 07:59:18 +0530

    initial commit

---
+++ src/main/java/com/kaw/research_hub/ResearchHubApplication.java
@@ -0,0 +1,13 @@
+package com.kaw.research_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaw/research_hub/ResearchHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaw.research_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

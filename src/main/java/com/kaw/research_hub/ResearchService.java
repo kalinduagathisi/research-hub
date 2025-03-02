5d2ab7315aeee2723e0d75a9f429d7adc9869db4
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-02 05:38:09 +0530

    implemented buildPrompt functionality

--- src/main/java/com/kaw/research_hub/ResearchService.java
+++ src/main/java/com/kaw/research_hub/ResearchService.java
@@ -14,6 +14,18 @@ public class ResearchService {
     }
 
     private String buildPrompt(ResearchRequest researchRequest) {
-        return null;
+        StringBuilder prompt = new StringBuilder();
+        switch (researchRequest.getOperation()) {
+            case "summarize:":
+                prompt.append("Provide a clear and concise summery of the following text in few sentences:\n\n");
+                break;
+            case "suggest":
+                prompt.append("Based on the following content, suggest related topics and further reading. Format the response with clear headings and bullet points:\n\n");
+                break;
+            default:
+                throw new IllegalArgumentException("Unkown Operation: " + researchRequest.getOperation());
+        }
+        prompt.append(researchRequest.getContent());
+        return prompt.toString();
     }
 }
